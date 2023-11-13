be7b66d9275f378cbced2911b267a60b2fd93de4
johnminton <johnminton04@gmail.com>
2023-11-13 22:49:28 +1300

    test push

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import yfinance as yf
+
 
 
 
@@ -9,5 +9,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Loser, you never even touched this damn project!')
 
