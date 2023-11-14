d7290d3aca6f32bebcd8189729098103243dac39
johnminton <johnminton04@gmail.com>
2023-11-14 13:40:44 +1300

    test push

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def other_func(name):
+    print('random other function')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Loser, you never even touched this damn project!')
+    other_func('eeee')
 
